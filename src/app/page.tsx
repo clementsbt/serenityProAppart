c29b438f36f6539fe757f56651cfe91a358528aa
Clément Subtil <clementsubtil2006@gmail.com>
2026-08-06 09:31:48 +0000

    fix: use Footer component on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import Navbar from "@/components/Navbar";
 import ContactCTA from "@/components/ContactCTA";
+import Footer from "@/components/Footer";
 import { useLanguage } from "@/context/LanguageContext";
 
 export default function Home() {
@@ -205,43 +206,7 @@ export default function Home() {
 
 
 
-      {/* Footer */}
-      <footer className="footer">
-        <div className="footer-grid">
-          <div>
-            <h4>{fr.common.siteName}</h4>
-            <p>
-              {fr.common.tagline}
-            </p>
-          </div>
-
-          <div>
-            <h4>{fr.footer.navigation}</h4>
-            <ul className="footer-links">
-              <li>
-                <a href="/">{fr.nav.home}</a>
-              </li>
-              <li>
-                <a href="#services">{fr.nav.services}</a>
-              </li>
-              <li>
-                <a href="#about">À propos</a>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <h4>{fr.footer.legal}</h4>
-            <ul className="footer-links">
-              <li>
-                <a href="/mentions-legales">{fr.footer.mentionsLegales}</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="footer-bottom">
-          <p>{fr.footer.copyright}</p>
-        </div>
-      </footer>
+      <Footer />
     </>
   );
 }
